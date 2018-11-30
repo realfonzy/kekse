f699f47aea9d69efa9d19b5dade3127553d2ba3b
realfonzy <45441021+realfonzy@users.noreply.github.com>
2018-11-30 22:15:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  echo $_POST['bla']
+?>
+<html>
+  <script type="text/javascript">
+    document.cookie = "macookie=asdfjkl";
+    document.write(document.cookie);
+  </script>
+  <form action="/index.php">
+    <input type="text" name="bla"><br />
+    <input type="submit" value="Submit">
+  </form>
+</html>
